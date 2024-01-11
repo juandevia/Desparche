7df7872929c4c9be44af06a5d2b4ca91ff3c7cbe
juandevia <juandevia03@gmail.com>
2024-01-11 13:40:22 -0800

    adds(+) Two functions incluiding an exercise from class

--- Python Sripts/HelloWorld.py
+++ Python Sripts/HelloWorld.py
@@ -1,6 +1,8 @@
 #Function Hello Wold!
+import math
+
 def HelloWorld():
-    print('Hello World!')
+    print( 'Hello World!')
 #Function reverse a vector or array
 def ReverseVector(a):
     #Variable i means the interactor and range the range that i´m moving in, so i´m moving between 0 and the size of the variable a  
@@ -15,6 +17,12 @@ def reverseString(string):
     print(reversed_string)
 
 
-reverseString("My name is Juan Camilo Devia Bastos")
+def Radiants2Degree(a):
+    degree=a*180/math.pi
+    print( a, " In degrees is ", degree)
+
+def ex1():
+    print( ' "$20 says he\'s not lying " ')
 
 
+Radiants2Degree(1)

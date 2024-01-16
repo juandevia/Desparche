e8e6eaa01f5c3879f0b0df97c69c837aab7d0404
juandevia <juandevia03@gmail.com>
2024-01-16 14:22:13 -0800

    Just added how to use Input function

--- Python Sripts/Print.py
+++ Python Sripts/Print.py
@@ -1,2 +1,13 @@
 print("This line uses simple quotes! \n\n\n", "This line uses double quotes... \n\n", "Lots of blank lines in between!",sep='')
 print("\t")
+
+
+def InputFun():
+    x = float (input(" Can you Input the x for the x = x (y + 1) "))
+    y = float (input(" Can you Input the y for the x = x (y + 1) "))
+    x *= y + 1
+    print( 'The result is', x)
+
+
+InputFun()
+
